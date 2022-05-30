a7d8320802d78e838737784866e3a40902a9714e
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2022-05-30 16:55:28 +0200

    feat : acceuil fonctionnel + produit url récupéré

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,16 @@
-fetch(`http://localhost:3000/api/products/${id}`)
+// Créer une variable de l'emplacement actuel de l'Url(search la partie après le ?)
+const urlSearch = new URLSearchParams(window.location.search);
+
+// Get lit l'id 
+const id = urlSearch.get("id");
+
+// Récupère les données de l'API de l'id choisi
+const fetchKanapProduct = async () => {
+  await fetch(`http://localhost:3000/api/products/${id}`)
+    .then((res) => res.json())
+    .then((data) => (kanap = data));
+
+  console.log(kanap);
+};
+
+fetchKanapProduct();
--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,48 @@
+// Récupère l'id de <section id="items">
 const items = document.getElementById("items");
 
-// const elementFactory = () => {
-
-// }
-
-// Créer une variable vide
-let kanapProduct = [];
+// Créer une variable vide qui stockent les éléments
+let kanaps = [];
 
 // Récupère les données de l'API
 const fetchKanap = async () => {
   await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => (kanapProduct = data));
+    .then((data) => (kanaps = data));
 
-  // console.log(kanapProduct);
+  // console.log(kanaps);
 };
 
+// Créer l'affichage des données reçu
 const kanapDisplay = async () => {
   await fetchKanap();
-  for (let i = 0; i < kanapProduct; i++) {
-    
-    let ancre = document.createElement("a");
-    ancre.href = "./product.html?id=";
-    document.querySelector(".items").appendChild(ancre);
 
+  for (let i = 0; i < 8; i++) {
+    const ancre = document.createElement("a");
+    const article = document.createElement("article");
+    const image = document.createElement("img");
+    const titre = document.createElement("h3");
+    const texte = document.createElement("p")
 
+    // Ancre
+    ancre.setAttribute("href", `./product.html?id=${kanaps[i]._id}`);
+    items.appendChild(ancre);
+    // Article
+    ancre.appendChild(article);
+    // Image
+    article.appendChild(image);
+    image.setAttribute('src', `${kanaps[i].imageUrl}`)
+    image.setAttribute('alt:', `${kanaps[i].altTxt}`)
+    // Titre
+    article.appendChild(titre);
+    titre.classList.add("productName")
+    titre.textContent = (`${kanaps[i].name}`)
+    // Texte
+    article.appendChild(texte)
+    texte.classList.add("productDescription")
+    texte.textContent = (`${kanaps[i].description}`)
   }
 };
 
+// Affiche les données
 kanapDisplay();
-
-// elementFactory();
