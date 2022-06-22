e41d7c4bf57481a42d9f407001e856dda27fe79e
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2022-06-22 16:25:33 +0200

    feat: changement de chemin + gestion panier

--- front/js/Controller.js
+++ front/js/Controller.js
@@ -27,14 +27,29 @@ class Controller {
     kanapView.render(kanap);
     // console.log(kanapView);
   }
+
+  // Ajoute un produit aux panier depuis le onclick:"controller.addItemToCart()"
+  addItemToCart() {
+    // Créer une variable de l'emplacement actuel de l'Url(search la partie après le ?)
+    const urlSearch = new URLSearchParams(window.location.search);
+
+    // Créer une constante Get qui lit l'id
+    const id = urlSearch.get("id");
+
+    // Créer une variable vide de l'input quantity
+    let selectedQuantity = document.getElementById("quantity")
+    let selectedColors = document.getElementById("colors")
+    
+    let quantity = +selectedQuantity.value
+    let color = selectedColors.value
+
+    let product = {}
+    product.id = id
+    product.quantity = quantity
+    product.color = color
+
+    let cart = new Cart(id, quantity, color);
+    cart.add(product)
+    cart.save();
+  }
 }
-
-const input = document.getElementById("addToCart")
-
-input.addEventListener("click", (e) => {
-  
-  let cart = new Cart()
-  
-  console.log(cart);
-  cart.save()
-})
--- front/js/Model/Cart.js
+++ front/js/Model/Cart.js
@@ -1,30 +1,6 @@
-// Model : gère les données d'une application (l'ajout, la modification, la suppression...)
-
-class Model {
-  // Récupère les données de l'API
-  async getListKanap() {
-    return fetch("http://localhost:3000/api/products")
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        return data;
-      });
-  }
-
-  // Récupère les données de l'API de l'id choisi
-  async getKanap(id) {
-    return fetch(`http://localhost:3000/api/products/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        return data;
-      });
-  }
-}
-
 class Cart {
   constructor() {
-  // Récupère le panier du local storage
+    // Récupère le panier du local storage
     let cart = localStorage.getItem("cart");
     if (cart == null) {
       this.cart = [];
@@ -40,12 +16,13 @@ class Cart {
 
   // Ajout un produit dans le panier
   add(product) {
-    let foundPoduct = this.cart.find((p) => p.id == product.id);
+    let selectedPoduct = this.cart.find(
+      (p) => p.id == product.id && p.color == product.color
+    );
 
-    if (foundPoduct != undefined) {
-      foundPoduct.quantity++;
+    if (selectedPoduct != undefined) {
+      selectedPoduct.quantity += product.quantity;
     } else {
-      product.quantity = 1;
       this.cart.push(product);
     }
     this.save();
@@ -59,12 +36,12 @@ class Cart {
 
   // Change une quantité d'un produit du panier
   changeQuantity(product, quantity) {
-    let foundPoduct = this.cart.find((p) => p.id == product.id);
+    let selectedPoduct = this.cart.find((p) => p.id == product.id);
 
-    if (foundPoduct != undefined) {
-      foundPoduct.quantity += quantity;
-      if (foundPoduct.quantity <= 0) {
-        this.remove(foundPoduct);
+    if (selectedPoduct != undefined) {
+      selectedPoduct.quantity += quantity;
+      if (selectedPoduct.quantity <= 0) {
+        this.remove(selectedPoduct);
       } else {
         this.save();
       }
